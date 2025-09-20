00bb8f780806e358c203a7b3e6d733b1a70f0d25
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-20 22:51:58 +0530

    working with json

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 8000;
 
-// With Static Page
-app.use(express.static(path.join(__dirname, "public")));
+/*
+// // With Static Page
+// app.use(express.static(path.join(__dirname, "public")));
+*/
 
+// Woring With Json
+const series = [
+  { id: 1, title: "Stranger Things" },
+  { id: 2, title: "Succession" },
+  { id: 3, title: "Breking Bad" },
+];
+app.get("/api/series", (req, res) => {
+  res.json(series);
+});
+
+/*
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "home.html"));
 // });
@@ -16,5 +29,6 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.get("/about", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "about.html"));
 // });
+*/
 
 app.listen(port, () => console.log(`App Listining On Port ${port}`));
