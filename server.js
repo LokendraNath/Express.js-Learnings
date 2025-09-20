75f0011a2954342c3ea01a8d2c0ac752eedecd52
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-20 22:20:11 +0530

    with about page

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.listen(port, () => console.log(`App Listining On Port ${port}`));
