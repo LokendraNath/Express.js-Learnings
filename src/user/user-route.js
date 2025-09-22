fd4684aab04159977089c149ba92ac4e5abaa9a8
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-22 16:15:48 +0530

    store the data

--- src/user/user-route.js
+++ src/user/user-route.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { User } from "./user-modle.js";
 
 const router = Router();
 
-router.post("/", (req, res) => {
-  //* yaha par "/" kyoki server me already /api/users dala hai to extra dalenge to extra ho jayega
-  res.json({ msg: "All good" });
+router.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
+  const result = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  console.log(result);
+
+  res.status(201).json({ id: result._id });
 });
 
 export default router;
