e6b0fcf815426b818a7d46ed4e11c28f32389994
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-22 11:48:41 +0530

    Middleware (Global)

--- cg-server.js
+++ cg-server.js
@@ -1,8 +1,17 @@
 import express from "express";
 const app = express();
 
-// JSON Data pars
+// JSON Data pars (Global Middle Ware)
 app.use(express.json());
+
+// Custom Middleware (Gloabal Middleware)
+const reqLogger = (req, res, next) => {
+  console.log(`${req.method} ${req.url} ${new Date().toISOString()}`);
+  // har req pe chalege -> POST /api/users 2025-09-22T06:15:56.132Z
+  next();
+};
+
+app.use(reqLogger);
 
 app.get("/", (req, res) => res.send("Hello World"));
 
@@ -10,7 +19,7 @@ app.get("/hello", (req, res) => res.send("route hello"));
 
 //Post Request
 app.post("/api/users", (req, res) => {
-  console.log("body", req.body.name);
+  console.log("body", req.body);
   res.json({});
 });
 
