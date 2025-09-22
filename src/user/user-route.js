cb10cb59ffe1e497bb3315c888216f85c1a507b8
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-22 22:13:01 +0530

    try catch throv next(error)

--- src/user/user-route.js
+++ src/user/user-route.js
@@ -11,14 +11,18 @@ const userValidation = [
     .withMessage("Password must be at least 6 chars"),
 ];
 
-router.post("/", userValidation, async (req, res) => {
+router.post("/", userValidation, async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty())
     return res.status(400).json({ status: "fail", errors: errors.array() });
 
   const { name, email, password } = req.body;
-  const user = await User.create({ name, email, password });
-  res.status(201).json({ status: "success", data: user });
+  try {
+    const user = await User.create({ name, email, password });
+    res.status(201).json({ status: "success", data: user });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
