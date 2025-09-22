03ca0b52b662d898209f223d2cef6564d7b4a715
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-22 10:51:56 +0530

    post request and parse data

--- cg-server.js
+++ cg-server.js
@@ -1,9 +1,18 @@
 import express from "express";
 const app = express();
 
+// JSON Data pars
+app.use(express.json());
+
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.get("/hello", (req, res) => res.send("route hello"));
+
+//Post Request
+app.post("/api/users", (req, res) => {
+  console.log("body", req.body.name);
+  res.json({});
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
