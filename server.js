70d5796304ef8a3c90b757336e1acbd315d33482
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-22 15:02:55 +0530

    rename the files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./cg-db.js";
+import { connectDB } from "./db.js";
 const app = express();
 
 // Connect Database
