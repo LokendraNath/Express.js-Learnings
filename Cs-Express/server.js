78924492134a1be8ed4f4b9666ec825edd25fe87
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 00:00:32 +0530

    handling form data using urlencoded

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -2,9 +2,17 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
+app.use(express.urlencoded());
+
 //* Simple Route
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+//* Handling Form Data
+app.post("/form", (req, res) => {
+  console.log(req.body);
+  res.send("Form Recieved");
 });
 
 app.listen(PORT, () => {
