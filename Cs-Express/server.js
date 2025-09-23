d320cc01dd1cf6712a35f0fee0353a2f830d6843
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-23 18:36:21 +0530

    put method learn

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -2,20 +2,41 @@ import express from "express";
 import router from "./route.js";
 const app = express();
 
-// Simple Route
+//* Simple Route
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-// jab bhi koi request /user se start hogi, usko us router file me bhej do.
+//* jab bhi koi request /user se start hogi, usko us router file me bhej do.
 app.use("/user", router);
 
-// Create Post Route
+//* Create Post Route
 app.post("/users", express.json(), (req, res) => {
   const { name, email } = req.body;
   res.json({
     message: `Welcome ${name} , your email is ${email}`,
   });
+});
+
+//* Create Put Method
+
+// Dummy data
+const dummyUsers = [
+  { id: 1, name: "Hritik" },
+  { id: 2, name: "Rohit" },
+  { id: 1, name: "Ronalod" },
+];
+
+app.put("/users/:id", express.json(), (req, res) => {
+  const userId = req.params.id;
+  const { name } = req.body;
+
+  const user = dummyUsers.find((u) => u.id === parseInt(userId));
+
+  if (!user) return res.status(400).send("User Not Found");
+
+  user.name = name;
+  res.send(name);
 });
 
 const PORT = 8000;
