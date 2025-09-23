85d2383eda143c8a486fbd7f88f83716073d6dcd
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-23 23:49:12 +0530

    file clean

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -2,10 +2,6 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
-//? app dekh sakte ho online file ko
-app.use(express.static("public"));
-app.use(express.static("images")); // Ek aur dalna hai to
-
 //* Simple Route
 app.get("/", (req, res) => {
   res.send("Hello");
