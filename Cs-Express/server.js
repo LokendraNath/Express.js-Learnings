77a1dd5bae369f8f39e98913ad4e8d2709cbff89
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-23 22:56:42 +0530

    clear file

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -7,20 +7,6 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
-//* Multiple Route Parameter
-app.get("/users/:userId/posts/:postId", (req, res) => {
-  const { userId, postId } = req.params;
-  res.send({
-    userId,
-    postId,
-  });
-});
-
-//* Catch-All Invalid Routes
-app.get((req, res) => {
-  res.send("Sorry, this is invalid URL");
-});
-
 app.listen(PORT, () => {
   console.log(`Listining on Port ${PORT}`);
 });
