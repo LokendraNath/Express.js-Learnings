3b2c84cb5cd7810b4ffd810935ccb90f8234d001
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 12:28:53 +0530

    form data file store

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import multer from "multer";
+import { storage } from "./config/multer.js";
 
 const app = express();
-const upload = multer();
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024000,
+  },
+});
 const PORT = 8000;
 
 app.use(express.urlencoded()); // for url encoded form data
-app.use(upload.array()); // for simple form data
+app.use(upload.single("image")); // for simple form data
 
 //* Simple Route
 app.get("/", (req, res) => {
@@ -16,6 +23,7 @@ app.get("/", (req, res) => {
 //* Handling Form Data
 app.post("/form", (req, res) => {
   console.log(req.body);
+  console.log(req.file);
   res.send("Form Recieved");
 });
 
