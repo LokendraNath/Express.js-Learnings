11a68d7b700df186d03e0f965a913d4677153753
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 13:25:58 +0530

    create schema and modle

---
+++ Cs-Express/models/User.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String,
+});
+
+export const User = mongoose.model("User", userSchema);
