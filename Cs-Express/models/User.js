bd0a08285a9f73954f67e47a8bf7f20d22295909
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 18:03:04 +0530

    add object type in schema and timestamp

--- Cs-Express/models/User.js
+++ Cs-Express/models/User.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  age: { type: Number, require: true },
-  email: { type: String, require: true, unique: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    age: { type: Number, require: true },
+    email: { type: String, require: true, unique: true },
+    userOrder: { type: Object, default: {} },
+  },
+  {
+    timestamps: true,
+    minimize: false,
+  }
+);
 
 export const User = mongoose.model("User", userSchema);
