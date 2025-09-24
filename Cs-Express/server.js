a6b217054f6ba1ebfe7b4ac4d980ffd828c182c7
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 13:50:38 +0530

    simple add user data in db using POST method

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import { User } from "./models/User.js";
 
 const app = express();
 const PORT = 8000;
@@ -8,6 +9,19 @@ await connectDB();
 //* Simple Route
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+//* Post Method
+app.post("/user", express.json(), async (req, res) => {
+  const { name, email, age } = req.body;
+  const newUser = new User({
+    name,
+    age,
+    email,
+  });
+  await newUser.save();
+  console.log(newUser);
+  res.send("User Added Successfully");
 });
 
 app.listen(PORT, () => {
