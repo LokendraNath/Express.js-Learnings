c9918bd850d0ccf3ba9180cea822f6a920e2057e
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 13:17:20 +0530

    connect to db

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -1,7 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8000;
+dotenv.config();
+const uri = process.env.MONGO_URI;
+
+//* Connect Database
+await mongoose.connect(uri).then(() => {
+  console.log("Connected To Database");
+});
 
 //* Simple Route
 app.get("/", (req, res) => {
