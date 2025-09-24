057237a16e0a6ace7ebb4aef16e9f495164cad03
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 12:31:15 +0530

    clear file

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -1,30 +1,11 @@
 import express from "express";
-import multer from "multer";
-import { storage } from "./config/multer.js";
 
 const app = express();
-
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 1024000,
-  },
-});
 const PORT = 8000;
-
-app.use(express.urlencoded()); // for url encoded form data
-app.use(upload.single("image")); // for simple form data
 
 //* Simple Route
 app.get("/", (req, res) => {
   res.send("Hello");
-});
-
-//* Handling Form Data
-app.post("/form", (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  res.send("Form Recieved");
 });
 
 app.listen(PORT, () => {
