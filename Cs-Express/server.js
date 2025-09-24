e7cef74dc0cc005414b801cd76b65e78e7b1e09b
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-24 16:57:20 +0530

    simple put methd to find userData

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -3,6 +3,7 @@ import { connectDB } from "./config/db.js";
 import { User } from "./models/User.js";
 
 const app = express();
+app.use(express.json());
 const PORT = 8000;
 await connectDB();
 
@@ -11,8 +12,8 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-//* Post Method
-app.post("/user", express.json(), async (req, res) => {
+//* Saving Data in MongoDB => Post Method
+app.post("/user", async (req, res) => {
   const { name, email, age } = req.body;
   const newUser = new User({
     name,
@@ -22,6 +23,14 @@ app.post("/user", express.json(), async (req, res) => {
   await newUser.save();
   console.log(newUser);
   res.send("User Added Successfully");
+});
+
+//* Update Data in MongoDB =>  PUT Method
+app.put("/user", async (req, res) => {
+  const { email, name } = req.body;
+  const matchUser = User.find({ email, name });
+  console.log(matchUser);
+  res.send("User Update Successfully");
 });
 
 app.listen(PORT, () => {
