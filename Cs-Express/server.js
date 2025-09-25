00ab45f02c1380d1e495dabc4a545e59d2cbac0e
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-25 17:23:23 +0530

    clear code

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -1,6 +1,4 @@
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 const app = express();
 
 //* Middleware
@@ -9,44 +7,6 @@ app.use(express.json());
 //* Simple Route
 app.get("/", (req, res) => {
   res.send("Hello");
-});
-
-const users = [];
-
-app.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  users.push({
-    username,
-    email,
-    password: hashedPassword,
-  });
-  res.send("User Registered");
-});
-
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = users.find((u) => u.email === email);
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.send("You are not authorized");
-  }
-  const token = jwt.sign(
-    { username: user.username, email: user.email },
-    "test#secret"
-  );
-  res.send({ token });
-});
-app.get("/dashboard", (req, res) => {
-  const authHeader = req.header("Authorization");
-  const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.send("no token provided");
-
-  try {
-    const decodedToken = jwt.verify(token, "test#secret");
-    res.send(`Welcom ${decodedToken.username}`);
-  } catch (error) {
-    res.send("Access Denied");
-  }
 });
 
 const PORT = 8000;
