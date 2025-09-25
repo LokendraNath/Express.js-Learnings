a93d6021d881756260407fc5a43d751a682595f9
Lokendra Nath <nathdeveloper25@gmail.com>
2025-09-25 11:30:46 +0530

    session managment

--- Cs-Express/server.js
+++ Cs-Express/server.js
@@ -1,14 +1,37 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import session from "express-session";
 
 const app = express();
 
 //* Middleware
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "sample-session",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //* Simple Route
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.get("/visit", (req, res) => {
+  if (req.session.page_views) {
+    //* multitime visited
+    req.session.page_views++;
+    res.send(`You Visited this page ${req.session.page_views} times`);
+  } else {
+    req.session.page_views = 1;
+    res.send("Welcome to this page for the first time !");
+  }
+});
+app.get("/remove-session", (req, res) => {
+  req.session.destroy();
+  res.send("Session will removed");
 });
 
 const PORT = 8000;
